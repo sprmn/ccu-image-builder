16c85586bfc53cf9a91a117b8d2604392af9ce6c
MrAlfabet <marijnblom86@gmail.com>
2020-10-26 15:11:00 +0100

    require sudo for build, remove docker image

--- create-ccu-image.sh
+++ create-ccu-image.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+docker rm -v pigen_work
+
 pushd stage2-custom/04-ccu-app/files
 rm -rf DCU-central-control-app
 git clone --single-branch --branch beta https://github.com/MrAlfabet/DCU-central-control-app.git

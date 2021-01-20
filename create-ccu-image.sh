362fdefd8636763589d919f0b8dfa3b33dcdd003
MrAlfabet <32557436+MrAlfabet@users.noreply.github.com>
2021-01-20 14:40:02 +0100

    grab temp-offset-control branches from github

--- create-ccu-image.sh
+++ create-ccu-image.sh
@@ -9,12 +9,12 @@ docker rm -v pigen_work
 
 pushd stage2-custom/04-ccu-app/files
 rm -rf DCU-central-control-app
-git clone --single-branch --branch beta https://github.com/MrAlfabet/DCU-central-control-app.git
+git clone --single-branch --branch temp-offset-control https://github.com/MrAlfabet/DCU-central-control-app.git
 popd
 
 pushd stage2-custom/06-ccu-server/files
 rm -rf ccu-server
-git clone --single-branch --branch beta https://github.com/sprmn/ccu-server.git
+git clone --single-branch --branch temp-offset-control https://github.com/sprmn/ccu-server.git
 popd
 
 ./build-docker.sh -c ccu.config
